f20bb02b983be56fb26fae66ae1c40f2451f9b90
Apenasgabs <gabers357@gmail.com>
2021-01-14 11:00:22 -0800

    estudando metodos http

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const teste = express();
 
+teste.get('/projects', (request, response) => {
+    return response.json([
+        'projeto 1',
+        'projeto 2'
+    ]);
+});
 
-teste
-
-
-teste.listen(3333);
+teste.post('/projects',(request, response) => {
+    return response.json([
+        'projeto 1',
+        'projeto 2',
+        'projeto 3'
+    ]);
+});
+teste.put('/projects:id',(request, response) => {
+    return response.json([
+        'projeto 4',
+        'projeto 2',
+        'projeto 3'
+    ]);
+});
+teste.listen(3333,() => {
+    console.log('Ainda ta rodando 🥴');
+});
